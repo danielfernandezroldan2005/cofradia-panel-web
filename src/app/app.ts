cc45458aa346981d2f58b38370945545d279a436
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-08-06 14:29:00 +0200

    Added initial configuration to the project.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './shared/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Sidebar], // Added Sidebar here.
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('cofradia-panel-web');
