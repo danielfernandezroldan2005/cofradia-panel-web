11092a100cc5b8eeaaa1f65d1bec538712ce94ad
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-08-06 14:37:55 +0200

    Installed Angular Material and created the first component (button).

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule], // Button in the initial screen.
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
