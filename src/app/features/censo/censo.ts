8f79a09d8b11c893ec7d4653da41a60b2edf4226
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-08-07 13:32:04 +0200

    Added a paginator.

--- src/app/features/censo/censo.ts
+++ src/app/features/censo/censo.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 // Define data structure that we want to show.
 export interface Hermano {
@@ -22,20 +23,41 @@ const HERMANOS_DATA: Hermano[] = [
 
 @Component({
   selector: 'app-censo',
-  imports: [MatTableModule, MatInputModule, MatFormFieldModule],
+  imports: [MatTableModule, MatInputModule, MatFormFieldModule, MatPaginatorModule],
   templateUrl: './censo.html',
   styleUrl: './censo.scss',
 })
-export class Censo {
+export class Censo implements AfterViewInit {
   // Display columns and order.
   displayedColumns: string[] = ['id', 'nombre', 'apellidos', 'fechaAlta', 'estado'];
 
   // Transform simple data in a DataSource of Material.
   dataSource = new MatTableDataSource(HERMANOS_DATA);
 
+  // Capture HTML paginator.
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  // When view is charged, we vinculate the paginator to the table.
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+
+    // We create a Personalized Filter by first and last names.
+    this.dataSource.filterPredicate = (data: Hermano, filter: string) => {
+      // Join first and last names in a string in lower case.
+      const textoFila = (data.nombre + ' ' + data.apellidos).toLowerCase();
+      // Check user string and compare with our data.
+      return textoFila.includes(filter);
+    };
+  }
+
   // Function executed when looking something in the filter bar.
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    // If we are looking for something, we return the paginator to the first page.
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
